fc90bf1f88a9081c62fac1e0a1c4d8f6bbac2bb8
nodo@lazypro.ge <nodardzamukashvili@outlook.com>
2020-12-03 21:40:03 +0400

    header added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import React from 'react';
-import {Route, Switch, BrowserRouter} from "react-router-dom";
-import ShopPage from './pages/shop/shop-page.component';
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import ShopPage from "./pages/shop/shop-page.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component = {HomePage}/>
-        <Route path='/shop' component = {ShopPage}/>
+    <div>
       
-      </Switch>
-    </BrowserRouter>
+      <BrowserRouter>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
