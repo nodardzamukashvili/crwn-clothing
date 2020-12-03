54dbd7fabef150b0852200afeadf5db82ae9c59a
nodo@lazypro.ge <nodardzamukashvili@outlook.com>
2020-12-03 21:04:04 +0400

    added shop preview

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import React from 'react';
 import {Route, Switch, BrowserRouter} from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-      <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop-page.component';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component = {HomePage}/>
-        <Route path='/hats' component = {HatsPage}/>
+        <Route path='/shop' component = {ShopPage}/>
+      
       </Switch>
     </BrowserRouter>
   );
