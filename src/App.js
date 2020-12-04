abb9e7824f949b0d0c740f70979c0a418ba4db5c
nodo@lazypro.ge <nodardzamukashvili@outlook.com>
2020-12-04 21:07:59 +0400

    added sign in visual

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import ShopPage from "./pages/shop/shop-page.component";
 import Header from "./components/header/header.component";
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
